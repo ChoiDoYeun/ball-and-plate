6424e1ab79d93d5f8d5d341aa8ea4fedbfa8d5a0
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-03-15 19:15:01 +0900

    Update servo_test.py

--- servo_test.py
+++ servo_test.py
@@ -10,13 +10,13 @@ max_pulse = 2500
 
 # 서보 모터를 순차적으로 움직이는 함수
 def move_servos_sequentially():
-    for i in range(4):  # 4개의 서보 모터 제어
+    for i in range(6):  # 4개의 서보 모터 제어
         kit.servo[i].set_pulse_width_range(min_pulse, max_pulse)
         kit.servo[i].angle = 0  # 0도로 서보 모터 회전
         time.sleep(1)
-        kit.servo[i].angle = 180  # 180도로 서보 모터 회전
+        kit.servo[i].angle = 20  # 180도로 서보 모터 회전
         time.sleep(1)
-        kit.servo[i].angle = 90  # 다시 90도로 서보 모터 회전 (중간 위치)
+        kit.servo[i].angle = 10  # 다시 90도로 서보 모터 회전 (중간 위치)
         time.sleep(1)
 
 if __name__ == '__main__':
